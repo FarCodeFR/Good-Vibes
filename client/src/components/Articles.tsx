87c65e023062c3fad11193725ac7fef23c03ea8c
vloiseau13 <valentin.loiseau13@gmail.com>
2024-12-05 21:44:16 +0100

    Card mapped on Home Page

--- client/src/components/Articles.tsx
+++ client/src/components/Articles.tsx
@@ -1,28 +1,26 @@
 import "../style/globals.css";
 import "../style/articles.css";
 
-function Articles() {
+interface ArticleProps {
+  keyword: string;
+  title: string;
+  image: string;
+  article: string;
+}
+
+function Articles({ keyword, title, image, article }: ArticleProps) {
   return (
     <>
       <section className="articles-main">
         <figure className="articles-figure">
-          <img src="src/assets/images/favicon.png" alt="" />
+          <img src={image} alt="" />
         </figure>
-        <article className="articles-articles">
-          <h3 className="articles-hashtag">#Société </h3>
-          <h4 className="articles-title">
-            Des chercheurs suisses redonnent espoir à des blessés à la moelle
-            épinière
-          </h4>
-          <p className="articles-content">
-            Un espoir pour certaines personnes atteintes de lésions de la moelle
-            épinière : la stimulation profonde de certaines zones du cerveau par
-            des électrodes pourrait les aider à marcher plus aisément, selon une
-            étude et un témoignage diffusés ce lundi par l’école polytechnique
-            fédérale de Lausanne.
-          </p>
+        <section className="articles-articles">
+          <h4 className="articles-hashtag">{keyword} </h4>
+          <h4 className="articles-title">{title}</h4>
+          <p className="articles-content">{article}</p>
           <p className="articles-link">Voir plus</p>
-        </article>
+        </section>
       </section>
     </>
   );
--- client/src/pages/Accueil.tsx
+++ client/src/pages/Accueil.tsx
@@ -1,20 +1,36 @@
+import { useLoaderData } from "react-router-dom";
 import Articles from "../components/Articles";
 import MainArticle from "../components/MainArticle.tsx";
 import "../style/globals.css";
 
+interface Article {
+  id: number;
+  keyword: string;
+  title: string;
+  image: string;
+  article: string;
+}
+
 function Accueil() {
+  const data = useLoaderData() as Article[];
+
   return (
     <>
       <main>
         <section className="home-articles">
           <MainArticle />
           <div className="scroll-articles">
-            <Articles />
-            <Articles />
-            <Articles />
-            <Articles />
-            <Articles />
-            <Articles />
+            {data?.map((element) => {
+              return (
+                <Articles
+                  key={element.id}
+                  keyword={element.keyword}
+                  image={element.image}
+                  article={element.article}
+                  title={element.title}
+                />
+              );
+            })}
           </div>
         </section>
       </main>
