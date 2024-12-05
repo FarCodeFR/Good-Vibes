b68f8d1de6fc5b67250fc729e7b4a0972ea3f0e6
FarCodeFR <Timo-44@hotmail.com>
2024-12-05 14:44:28 +0100

    Resolution probleme global.css, footer.css, header.css et renomage balise header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,28 +1,31 @@
 import { Link } from "react-router-dom";
+import "../../style/globals.css";
 import "./header.css";
 
 function Header() {
   return (
-    <nav className="header-container-link">
+    <header className="header-container-link">
       <img
         src="/src/assets/images/Logo.png"
         alt="logo good vibes l'actualité qui fait du bien"
       />
-      <ul className="link-container-page">
-        <Link to="/accueil">
-          <li>Accueil</li>
-        </Link>
-        <Link to="/societe">
-          <li>Société</li>
-        </Link>
-        <Link to="/environnement">
-          <li>Environnement</li>
-        </Link>
-        <Link to="/contact">
-          <li>Contact</li>
-        </Link>
-      </ul>
-    </nav>
+      <nav className="link-container-page">
+        <ul>
+          <Link to="/accueil">
+            <li>Accueil</li>
+          </Link>
+          <Link to="/societe">
+            <li>Société</li>
+          </Link>
+          <Link to="/environnement">
+            <li>Environnement</li>
+          </Link>
+          <Link to="/contact">
+            <li>Contact</li>
+          </Link>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
