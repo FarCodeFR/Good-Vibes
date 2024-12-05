cf996924aae7faa77b92fec134eaf9bb076d39bc
ViriatoF <viriato.ferreira44@gmail.com>
2024-12-05 13:43:21 +0100

    Change order of import in app for resolve merge conflicts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header";
 import "./style/globals.css";
+import Header from "./components/Header";
 
 function App() {
   return (
