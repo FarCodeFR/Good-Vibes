5dd7143bcfb29201715ded1bf810e6f3da494047
ViriatoF <viriato.ferreira44@gmail.com>
2024-12-05 11:00:07 +0100

    Commit for get the styles folder

--- client/src/pages/contact.tsx
+++ client/src/pages/contact.tsx
@@ -1,21 +1,22 @@
-function contact() {
+function Contact() {
   return (
-    <section>
+    <section id="display-form">
       <h1>Contacte-nous</h1>
-      <p>Des bonnes nouvelles à nous partager?</p>
-      <p>Ecris nous un message</p>
+      <h3>Des bonnes nouvelles à nous partager?</h3>
+      <p>Ecris-nous un message</p>
+
       <form action="">
         <label htmlFor="">Prénom</label>
         <input type="text" placeholder="Ton p'tit nom mon mignon" required />
 
         <label htmlFor="">E-mail</label>
-        <input type="email" placeholder="ton-email@gmail.com" required />
+        <input type="email" placeholder="mail@mail.com" required />
 
         <label htmlFor="">Votre message</label>
-        <input type="text" placeholder="Tapez votre message ici..." />
+        <input type="text-area" placeholder="Ton message" required />
       </form>
     </section>
   );
 }
 
-export default contact;
+export default Contact;
