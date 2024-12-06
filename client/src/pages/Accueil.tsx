fcc3664cfc9a2d96e3288325ed42515e078d3ec4
vloiseau13 <valentin.loiseau13@gmail.com>
2024-12-06 14:00:51 +0100

    Responsive done

--- client/src/pages/Accueil.tsx
+++ client/src/pages/Accueil.tsx
@@ -4,39 +4,39 @@ import MainArticle from "../components/MainArticle.tsx";
 import "../style/globals.css";
 
 interface Article {
-	id: number;
-	keyword: string;
-	title: string;
-	image: string;
-	article: string;
+  id: number;
+  keyword: string;
+  title: string;
+  image: string;
+  article: string;
 }
 
 function Accueil() {
-	const data = useLoaderData() as Article[];
+  const data = useLoaderData() as Article[];
 
-	return (
-		<>
-			<main className="home-page">
-				<section className="home-articles">
-					<MainArticle />
-					<hr className="tiret-accueil" />
-					<div className="scroll-articles">
-						{data?.map((element) => {
-							return (
-								<Articles
-									key={element.id}
-									keyword={element.keyword}
-									image={element.image}
-									article={element.article}
-									title={element.title}
-								/>
-							);
-						})}
-					</div>
-				</section>
-			</main>
-		</>
-	);
+  return (
+    <>
+      <main className="home-page">
+        <section className="home-articles">
+          <MainArticle />
+          <hr className="tiret-accueil" />
+          <div className="scroll-articles">
+            {data?.map((element) => {
+              return (
+                <Articles
+                  key={element.id}
+                  keyword={element.keyword}
+                  image={element.image}
+                  article={element.article}
+                  title={element.title}
+                />
+              );
+            })}
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default Accueil;
